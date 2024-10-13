5650624361f71a4dc9408693dee1cf041160b04b
Zeblanc <timmothyssompanit@gmail.com>
2024-10-13 15:31:03 -0700

    clean some lines

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AccountMatchHistory from "./components/AccountMatchHistory";
 import RiotAccount from "./components/RiotAccount";
 import Banner from "./components/Banner";
