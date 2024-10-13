caa3876a00a461648b0bef1a44efbf6eaa47342d
Zeblanc <timmothyssompanit@gmail.com>
2024-10-13 15:23:46 -0700

    Fixed Banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import AccountMatchHistory from "./components/AccountMatchHistory";
 import RiotAccount from "./components/RiotAccount";
 import Banner from "./components/Banner";
@@ -7,34 +7,27 @@ import Home from './pages/Home';
 import Leaderboard from "./pages/Leaderboard";
 import Login from "./pages/Login";
 
-
 function App() {
   const [puuid, setPuuid] = useState(null);
 
-
-
   return (
-    <>
-      <Router>
+    <Router>
+      <div>
         <Banner />
-  
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
           <Route path="/leaderboards" element={<Leaderboard />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/account" element={
+            <div className="container text-center">
+              <RiotAccount setPuuid={setPuuid} />
+              <AccountMatchHistory puuid={puuid} />
+            </div>
+          } />
+          
         </Routes>
-
-      
-      <div className="container text-center">
-        <RiotAccount setPuuid={setPuuid}/>
-        <AccountMatchHistory puuid={puuid}/>
       </div>
-
-
-      </Router>
-
-
-    </>
+    </Router>
   );
 }
 
